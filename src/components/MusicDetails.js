aec2918044474822308581489c25667c3e49d021
Charles <ellerkaresz@gmail.com>
2018-11-15 23:26:57 +0100

    Display the details of the music

--- src/components/MusicDetails.js
+++ src/components/MusicDetails.js
@@ -1,11 +1,46 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
-const MusicPlayer = () => {
+const MusicDetails = (props) => {
+      let {
+            musicName,
+            artistName,
+            albumName,
+            genres,
+            genreDescriptions,
+            seconds
+      } = props.state.searchResult;
+      let genreIndex = -1;
+
       return(
             <div className="music-details container">
-                  music details
+                  {
+                        typeof musicName === 'undefined' ? null :
+                        (
+                              <div>
+                                    <h1>{musicName} by <strong id="artist-name">{artistName}</strong></h1>
+                                    <h2>Album: {albumName}</h2>
+                                    <ul>
+                                          Genres:
+                                          {
+                                                genres.map(genre => {    
+                                                      genreIndex++;
+                                                      return <li key={genre} title={genreDescriptions[genreIndex]}>{genre}</li>
+                                                })
+                                          }
+                                    </ul>
+                                    <strong>Length: {seconds} seconds</strong>
+                              </div>
+                        )
+                  }
             </div>
       )
 }
 
-export default MusicPlayer;
+const mapStateToProps = (state) => {
+      return {
+            state
+      }
+}
+
+export default connect(mapStateToProps)(MusicDetails);
--- src/components/MusicPlayer.js
+++ src/components/MusicPlayer.js
@@ -1,13 +1,38 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
 
 class MusicPlayer extends Component {
+      componentWillReceiveProps(){
+            let musicPlayer = document.querySelector('.music-player');
+            
+            let newAudioTag = document.createElement('audio');
+            newAudioTag.setAttribute('controls', '');
+
+            let newSourceTag = document.createElement('source');
+            newSourceTag.src = this.props.state.searchResult.url;
+            newSourceTag.type = 'audio/mp3';
+
+            newAudioTag.appendChild(newSourceTag);
+
+            console.log('received props', newAudioTag, this.props);
+            
+            musicPlayer.children[0].remove();
+            musicPlayer.appendChild(newAudioTag);
+      }
+      
       render(){
             return(
                   <div className="music-player container">
-                        music player
+                        <audio controls></audio>
                   </div>
             )
       }
 }
 
-export default MusicPlayer;
+const mapStateToProps = (state) => {
+      return {
+            state
+      }
+}
+
+export default connect(mapStateToProps)(MusicPlayer);
