9a5dc0c4d06f4d79d8c7226844ac739a85d313c1
Charles <ellerkaresz@gmail.com>
2018-11-25 18:54:14 +0100

    Updates

---
+++ precache-manifest.2bbe438c55e7a12d0e775705f981e0ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "98b7c4d9a79ccb5c14ef",
+    "url": "/static/js/main.98b7c4d9.chunk.js"
+  },
+  {
+    "revision": "6e12758b854aebd903ff",
+    "url": "/static/js/1.6e12758b.chunk.js"
+  },
+  {
+    "revision": "98b7c4d9a79ccb5c14ef",
+    "url": "/static/css/main.f625ae69.chunk.css"
+  },
+  {
+    "revision": "48d60e50829a02fa381378767e7c0553",
+    "url": "/index.html"
+  }
+];
